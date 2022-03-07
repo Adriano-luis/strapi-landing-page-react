21c5b989fcefeb5ca630795d22b9ed45a9ed4c2f
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-03-07 07:53:20 -0300

    criar NavLink

---
+++ src/components/NavLinks/NavLinks.test.jsx
@@ -0,0 +1,33 @@
+import { renderTheme } from '../../styles/render-theme';
+import { NavLinks } from '.';
+import { screen } from '@testing-library/react';
+import mock from './mock';
+import { theme } from '../../styles/theme';
+
+describe('<NavLinks />', () => {
+  it('should render links', () => {
+    renderTheme(<NavLinks links={mock} />);
+    expect(screen.getAllByRole('link')).toHaveLength(mock.length);
+  });
+
+  it('should not render Links', () => {
+    renderTheme(<NavLinks />);
+    expect(screen.queryAllByText(/link/i)).toHaveLength(0);
+  });
+
+  it('should not render Links', () => {
+    renderTheme(<NavLinks links={mock} />);
+    expect(screen.getByText(/link 10/i).parentElement).toHaveStyleRule(
+      'flex-flow',
+      'column wrap',
+      {
+        media: theme.media.lteMedium,
+      },
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<NavLinks links={mock} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
---
+++ src/components/NavLinks/index.jsx
@@ -0,0 +1,23 @@
+import * as Style from './styles';
+import P from 'prop-types';
+import { MenuLink } from '../MenuLink';
+
+export const NavLinks = ({ links = [] }) => {
+  return (
+    <Style.Container>
+      {links.map((link) => (
+        <MenuLink key={link.link} {...link} />
+      ))}
+    </Style.Container>
+  );
+};
+
+NavLinks.propTypes = {
+  links: P.arrayOf(
+    P.shape({
+      children: P.string.isRequired,
+      link: P.string.isRequired,
+      newTab: P.bool,
+    }),
+  ),
+};

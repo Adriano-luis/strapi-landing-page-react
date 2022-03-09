5176b503fc65ca12556c7696bbd30d98b3309f30
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-03-09 19:10:30 -0300

    cridado GridText

---
+++ src/components/GridSection/mock.js
@@ -0,0 +1,35 @@
+export default {
+  background: false,
+  title: 'My grid',
+  description:
+    'Uma breve descrição colocada aqui para teste e ver como que fica',
+  grid: [
+    {
+      title: 'Teste 1',
+      description: `Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit. Morbi dapibus risus 
+      vel dolor lobortis finibus. Cras egestas ipsum et 
+      posuere dictum. Vestibulum ornare nisi at congue laoreet. 
+      Pellentesque sit amet velit sapien. Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit.`,
+    },
+    {
+      title: 'Teste 2',
+      description: `Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit. Morbi dapibus risus 
+      vel dolor lobortis finibus. Cras egestas ipsum et 
+      posuere dictum. Vestibulum ornare nisi at congue laoreet. 
+      Pellentesque sit amet velit sapien. Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit.`,
+    },
+    {
+      title: 'Teste 3',
+      description: `Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit. Morbi dapibus risus 
+      vel dolor lobortis finibus. Cras egestas ipsum et 
+      posuere dictum. Vestibulum ornare nisi at congue laoreet. 
+      Pellentesque sit amet velit sapien. Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit.`,
+    },
+  ],
+};

1ac8ae457f3c7e7aed6f17029cca4faaf5da7e4c
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-03-10 07:49:21 -0300

    corrigido  GridText e headings

---
+++ src/components/GridText/index.jsx
@@ -0,0 +1,40 @@
+import * as Style from './styles';
+import P from 'prop-types';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridText = ({ title, description, grid, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Style.Container>
+        <Heading size="huge" uppercase colorDark={!background} as="h2">
+          {title}
+        </Heading>
+        <TextComponent>{description}</TextComponent>
+        <Style.Grid>
+          {grid.map((el) => (
+            <Style.GridElement key={el.title}>
+              <Heading size="medium" colorDark={!background} as="h3">
+                {el.title}
+              </Heading>
+              <TextComponent>{el.description}</TextComponent>
+            </Style.GridElement>
+          ))}
+        </Style.Grid>
+      </Style.Container>
+    </SectionBackground>
+  );
+};
+
+GridText.propTypes = {
+  title: P.string.isRequired,
+  background: P.bool,
+  description: P.string.isRequired,
+  grid: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      description: P.string.isRequired,
+    }),
+  ).isRequired,
+};
